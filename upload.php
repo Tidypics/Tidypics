21ac24fcb37ded0a91ed32051936c93cc48fa002
Cash Costello <cash.costello@gmail.com>
2009-03-21 15:59:18 +0000

    more cleanup

--- upload.php
+++ upload.php
@@ -10,26 +10,26 @@
 	
 	
 	// Get the current page's owner
-		if ($album = (int) get_input('container_guid')) 
-		{
-			$album_entity = get_entity($album);
-			
-			//if album does not exist or user does not have access
-			if(!$album_entity || !$album_entity->canEdit())
-				forward('pg/photos/owned/');
-			
-			//set group to "real" container
-			$container = $album_entity->container_guid;				
-			set_page_owner($container);
-		}
-		else
+	if ($album = (int) get_input('container_guid')) 
+	{
+		$album_entity = get_entity($album);
+	
+		//if album does not exist or user does not have access
+		if(!$album_entity || !$album_entity->canEdit())
 			forward('pg/photos/owned/');
+			
+		//set group to "real" container
+		$container = $album_entity->container_guid;				
+		set_page_owner($container);
+	}
+	else
+		forward('pg/photos/owned/');
 				
-		$page_owner = page_owner_entity();
-		if ($page_owner === false || is_null($page_owner)) {
-			$page_owner = $_SESSION['user'];
-			set_page_owner($page_owner->getGUID());
-		}
+	$page_owner = page_owner_entity();
+	if ($page_owner === false || is_null($page_owner)) {
+		$page_owner = $_SESSION['user'];
+		set_page_owner($page_owner->getGUID());
+	}
 		
 	set_context('photos');
 	$title = elgg_echo('album:addpix') . ' ' . $album_entity->title;
