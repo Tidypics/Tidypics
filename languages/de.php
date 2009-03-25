1cfccdbc80a77a8c8713684758b24cd4c06ea136
alfalive <dkfree@gmx.de>
2009-03-25 09:39:09 +0000

    Some improvements for better understanding. v1.3

--- languages/de.php
+++ languages/de.php
@@ -1,12 +1,13 @@
 <?php
 	/**
-	 * Elgg tidypics plugin language pack
-	 * 
-	 * @author Curverider Ltd
+	 * Elgg tidypics plugin german language pack
+	 *
+     * @version 1.3 (Changes in this version: Improvements for better understanding)
+	 * @author  Modified and fixed by Dieter Konrad aka alfalive
+     * @contact http://community.elgg.org/pg/profile/alfalive
 	 * @copyright Curverider Ltd 2008
 	 * @link http://elgg.com/
      *
-     *  FIXED problem for german letters umlaute äöü and ÄÖÜ     Importand! (File is stored in UTF-8 Without BOM)
 	 */
 
 	$german = array(
@@ -28,7 +29,7 @@
 			'album:group' => "Alben der Gruppe",
 			'item:object:image' => "Photos",
 			'item:object:album' => "Alben",
-			'tidypics:settings:maxfilesize' => "Maximale Dateigröße in den Kilobytes (KB):",
+			'tidypics:settings:maxfilesize' => "Maximale Dateigröße in Kilobytes (KB):",
 	
 		//actions
 		
@@ -40,7 +41,7 @@
 
 			'image:edit' => "Bild bearbeiten",
 			'image:delete' => "Bild löschen",
-			'image:download' => "Download Bild",
+			'image:download' => "Bild herunterladen",
 		
 		//forms
 		
@@ -49,14 +50,14 @@
 			'album:tags' => "Stichwörter",
 			'album:cover' => "Albumcover erstellen?",
 			'album:cover:yes' => "Ja",
-			'image:access:note' => "(Zugriffsberechtigung wird vom Album übernommen)",
+			'image:access:note' => "Info: Zugriffsberechtigung wird vom Album übernommen",
 			
 		//views 
 		
 			'image:total' => "Bilder im Album:",
 			'image:by' => "Bild hinzugefügt von",
 			'album:by' => "Album erstellt von:",
-			'album:created:on' => "Verursacht",
+			'album:created:on' => "Erstellt",
 			'image:none' => "Noch keine Bilder hinzugefügt.",
 			'image:back' => "Zurück",
 			'image:next' => "Weiter",
@@ -86,13 +87,13 @@
 			'image:saved' => "Dein Bild wurde gespeichert.",
 			'images:saved' => "Alle Bilder wurden gespeichert.",
 			'image:deleted' => "Dein Bild wurde gelöscht.",
-			'image:delete:confirm' => "Willst Du das Bild wirklich löschen?",
+			'image:delete:confirm' => "Willst du das Bild wirklich löschen?",
 			
 			'images:edited' => "Dein Bild wurde aktualisiert.",
 			'album:edited' => "Dein Album wurde aktualisiert.",
 			'album:saved' => "Dein Album wurde gespeichert.",
 			'album:deleted' => "Dein Album wurde gelöscht.",
-			'album:delete:confirm' => "Willst Du das Album wirklich löschen?",
+			'album:delete:confirm' => "Willst du das Album wirklich löschen?",
 			'album:created' => "Ihr neues Album ist hergestellt worden.",
 			'tidypics:status:processing' => "Warten Sie bitte, während wir verarbeiten Ihre Abbildung....",
 				
@@ -107,9 +108,9 @@
 			'images:notedited' => 'Nicht alle Bilder konnten hochgeladen werden',
 		 
 			'album:none' => "Keine Alben gefunden.",
-			'album:uploadfailed' => "Sorry; Dein Album konnte nicht gespeichert werden.",
+			'album:uploadfailed' => "Dein Album konnte nicht gespeichert werden.",
 			'album:deletefailed' => "Dein Album konnte nicht gelöscht werden.",
-			'album:blank' => "Geben Sie diesem Album einen Titel und eine Beschreibung bitte."
+			'album:blank' => "Gib diesem Album einen Titel und eine Beschreibung bitte."
 	);
 					
 	add_translation("de",$german);
