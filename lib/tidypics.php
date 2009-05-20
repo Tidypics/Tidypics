861c1143e231edce5e3b1fc537fe52c51e39bbc6
Cash Costello <cash.costello@gmail.com>
2009-05-20 01:00:39 +0000

    added function for determining path to main image directory

--- lib/tidypics.php
+++ lib/tidypics.php
@@ -4,5 +4,17 @@
 	 * 
 	 */
 
-
+	/**
+	 * Get image directory path
+	 *
+	 * Each album gets a subdirectory based on its container id
+	 *
+	 * @return string	path to image directory
+	 */
+	function tp_get_img_dir()
+	{
+		$file = new ElggFile();
+		return $file->getFilenameOnFilestore() . 'image/';
+	}
+	
 ?>
