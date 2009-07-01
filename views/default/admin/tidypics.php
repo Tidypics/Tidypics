d6d3d6e1b6740b3f9dd437219755ba0fa02fa185
Cash Costello <cash.costello@gmail.com>
2009-07-01 11:55:01 +0000

    fixed extra ' in administration view

--- views/default/admin/tidypics.php
+++ views/default/admin/tidypics.php
@@ -19,7 +19,7 @@
 <div class="contentWrapper">
 	<div id="elgg_horizontal_tabbed_nav">
 		<ul>
-			<li <?php echo $settingsselect; ?>'><a href="<?php echo $CONFIG->wwwroot . 'mod/tidypics/admin.php?tab=settings'; ?>"><?php echo elgg_echo('tidypics:settings'); ?></a></li>
+			<li <?php echo $settingsselect; ?>><a href="<?php echo $CONFIG->wwwroot . 'mod/tidypics/admin.php?tab=settings'; ?>"><?php echo elgg_echo('tidypics:settings'); ?></a></li>
 			<li <?php echo $statsselect; ?>><a href="<?php echo $CONFIG->wwwroot . 'mod/tidypics/admin.php?tab=stats'; ?>"><?php echo elgg_echo('tidypics:stats'); ?></a></li>
 		</ul>
 	</div>
