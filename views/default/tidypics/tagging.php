46fdfa767062fa42c6ec3dfc6f985afc5ccee08b
Cash Costello <cash.costello@gmail.com>
2009-07-02 11:37:05 +0000

    sorting friends list and going beyond 10

--- views/default/tidypics/tagging.php
+++ views/default/tidypics/tagging.php
@@ -30,7 +30,14 @@
 <?php
 
 	if($viewer) {
-		$friends = get_entities_from_relationship('friend', $viewer->getGUID(), false, 'user', '', 0);
+		$friends = get_entities_from_relationship('friend', $viewer->getGUID(), false, 'user', '', 0, 'time_created desc', 1000);
+		
+		if ($friends) {
+			foreach($friends as $friend) {
+				$friend_array[$friend->name] = $friend->getGUID();
+			}
+		}
+		ksort($friend_array);
 
 		$content = "<input type='hidden' name='image_guid' value='{$file_guid}' />";
 		$content .= "<input type='hidden' name='coordinates' id='coordinates' value='' />";
@@ -41,8 +48,8 @@
 		$content .= "<li><a href='javascript:void(0)' onclick='selectUser({$viewer->getGUID()},\"{$viewer->name}\")'> {$viewer->name} (" . elgg_echo('me') . ")</a></li>";
 	
 		if ($friends) {
-			foreach($friends as $friend) {
-				$content .= "<li><a href='javascript:void(0)' onclick='selectUser({$friend->getGUID()}, \"{$friend->name}\")'>{$friend->name}</a></li>";
+			foreach($friend_array as $friend_name => $friend_guid) {
+				$content .= "<li><a href='javascript:void(0)' onclick='selectUser({$friend_guid}, \"{$friend_name}\")'>{$friend_name}</a></li>";
 			}
 		}
 	}
