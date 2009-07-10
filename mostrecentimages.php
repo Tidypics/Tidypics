ef40f4101aa5653a6697a273806ed4d4537bc91e
Cash Costello <cash.costello@gmail.com>
2009-07-10 23:49:33 +0000

    mostly fixed recent images listing - issue with css on pagination when surrounding floated elements

--- mostrecentimages.php
+++ mostrecentimages.php
@@ -1,8 +1,7 @@
 <?php
 
 	/**
-	 * Tidypics full view of an image
-	 * Given a GUID, this page will try and display any entity
+	 * Most recent uploaded images
 	 * 
 	 */
 
@@ -12,16 +11,11 @@
 	$max = 8;
 	$images = list_entities("object", "image", 0, $max, false, false, true);
 	
-	$title = "Most recent images";
+	$title = elgg_echo('tidypics:mostrecent');
 	$area2 = elgg_view_title($title);
-	
-//	include_once(dirname(__FILE__) . "/notice.php");
-//	$notice = tp_notice(); 
-//	$area2 .= $notice->text;
-	
+	$area2 .= '<div class="contentWrapper">'; 
 	$area2 .= $images;
+	$area2 .= '</div>';
 	$body = elgg_view_layout('two_column_left_sidebar', '', $area2);
 	page_draw($title, $body);
-	if($notice->showMessage == true) system_message($notice->text);
-	
 ?>
