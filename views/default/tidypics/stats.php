bbd3f9d8566a7050784d1adbbbbe84ab6521ec19
Cash Costello <cash.costello@gmail.com>
2009-07-17 01:35:46 +0000

    simple stats

--- views/default/tidypics/stats.php
+++ views/default/tidypics/stats.php
@@ -1,3 +1,14 @@
 <?php
-	echo 'Stats go here';
+
+	$img_type = get_subtype_id('object', 'image');
+	$query = "SELECT count(guid) as total from {$CONFIG->dbprefix}entities where subtype={$img_type}";
+	$total = get_data_row($query);
+	$num_images = $total->total;
+	
+	$img_type = get_subtype_id('object', 'album');
+	$query = "SELECT count(guid) as total from {$CONFIG->dbprefix}entities where subtype={$img_type}";
+	$total = get_data_row($query);
+	$num_albums = $total->total;
+
+	echo  $num_images . " Photos in " . $num_albums . " Albums";
 ?>
