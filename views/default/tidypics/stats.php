0a0fc516dcc7a7770d89c8e8a5dbb2e578850551
Cash Costello <cash.costello@gmail.com>
2009-07-30 11:38:40 +0000

    added more stats

--- views/default/tidypics/stats.php
+++ views/default/tidypics/stats.php
@@ -10,5 +10,26 @@
 	$total = get_data_row($query);
 	$num_albums = $total->total;
 
-	echo  $num_images . " Photos in " . $num_albums . " Albums";
+	$num_comments_photos = count_annotations(0, 'object', 'image', 'generic_comment');
+	$num_comments_albums = count_annotations(0, 'object', 'album', 'generic_comment');
+	
+	$num_views = count_annotations(0, 'object', 'image', 'tp_view');
+	
+	if (get_plugin_setting('tagging', 'tidypics') != "disabled")
+		$num_tags = count_annotations(0, 'object', 'image', 'phototag');
 ?>
+<p>
+<br />
+Photos: <?php echo $num_images; ?><br />
+Albums: <?php echo $num_albums; ?><br />
+Comments on photos: <?php echo $num_comments_photos; ?><br />
+Comments on albums: <?php echo $num_comments_albums; ?><br />
+Total views: <?php echo $num_views; ?><br />
+<?php 
+	if ($num_tags) {
+?>
+Photo tags: <?php echo $num_tags; ?><br />
+<?php
+	}
+?>
+</p>
