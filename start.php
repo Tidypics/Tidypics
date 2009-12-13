066a30d42aa814637facc30f5cc24e8a50d908d8
Cash Costello <cash.costello@gmail.com>
2009-12-13 02:13:13 +0000

    1.6.5 version

--- start.php
+++ start.php
@@ -189,10 +189,11 @@
 			add_submenu_item(	elgg_echo('tidypics:recentlycommented'),
 								$CONFIG->wwwroot . 'pg/photos/recentlycommented/',
 								'tidypics-z');
+/*
 			add_submenu_item(	'Flickr Integration',
 								$CONFIG->wwwroot . 'mod/tidypics/pages/flickr/setup.php',
 								'tidypics-z');
-
+*/
 		}
 		
 	}
