d77e8fd4cb76560ae2f1c180cc9ccbcde7e3942a
Cash Costello <cash.costello@gmail.com>
2010-07-30 02:15:39 +0000

    added methods for managing the order of photos in the album

--- lib/album.php
+++ lib/album.php
@@ -16,4 +16,39 @@ class TidypicsAlbum extends ElggObject {
 	public function __construct($guid = null) {
 		parent::__construct($guid);
 	}
+
+	/**
+	 * Returns an order list of image guids
+	 * 
+	 * @return array
+	 */
+	public function getOrderedImageList() {
+		$listString = $this->orderedImages;
+		if (!$listString) {
+			return array();
+		}
+		$list = unserialize($listString);
+		return $list;
+	}
+
+	/**
+	 * Sets the album image order
+	 *
+	 * @param array $list An indexed array of image guids 
+	 */
+	public function setOrderedImageList($list) {
+		$listString = serialize($list);
+		$this->orderedImages = $listString;
+	}
+
+	/**
+	 * Add new images to the front of the image list
+	 *
+	 * @param array $list An indexed array of image guids
+	 */
+	public function prependOrderedImageList($list) {
+		$currentList = $this->getOrderedImageList();
+		$list = array_merge($list, $currentList);
+		$this->setOrderedImageList($list);
+	}
 }
