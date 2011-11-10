eb9b13e1bfd861cba9b7a812d929b565d5775175
Cash Costello <cash.costello@gmail.com>
2011-11-10 07:33:15 -0500

    removed more flickr code

--- lib/flickr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function flickr_menu() {
-	add_submenu_item( elgg_echo( 'flickr:menusetup' ), "/mod/tidypics/pages/flickr/setup.php");
-	add_submenu_item( elgg_echo( 'flickr:menuimport' ), "/mod/tidypics/pages/flickr/importPhotosets.php" );
-}
