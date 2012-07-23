0e68c2dd6f54ca26b55bb09ea35d4e8a36bc7925
Kevin Jardine <kevinjardine@yahoo.com>
2012-07-23 12:33:51 +0200

    got a reasonable river item working

--- actions/photos/image/tag.php
+++ actions/photos/image/tag.php
@@ -74,18 +74,20 @@ $tag->type = $relationships_type;
 $tag->value = $value;
 $access_id = $image->getAccessID();
 
+$tagger = elgg_get_logged_in_user_entity();
+
 $annotation_id = $image->annotate('phototag', serialize($tag), $access_id);
 if ($annotation_id) {
 	// if tag is a user id, add relationship for searching (find all images with user x)
 	if ($tag->type === 'user') {
 		if (!check_entity_relationship($tag->value, 'phototag', $image_guid)) {
 			add_entity_relationship($tag->value, 'phototag', $image_guid);
-/*
-			// also add this to the river - subject is image, object is the tagged user
-			add_to_river('river/object/image/tag', 'tag', $tagger->guid, $user_id, $access_id, 0, $annotation_id);
+
+			// also add this to the river - subject is tagger, object is the image
+			add_to_river('river/object/image/tag', 'tag', $tagger->guid, $image_guid, $access_id, 0, $annotation_id);
 
 			// notify user of tagging as long as not self
-			if ($owner_id != $user_id) {
+			/*if ($owner_id != $user_id) {
 				notify_user(
 						$user_id,
 						$owner_id,
@@ -97,9 +99,7 @@ if ($annotation_id) {
 							$image->getURL()
 						)
 				);
-			}
- * 
- */
+			}*/
 		}
 	}
 
