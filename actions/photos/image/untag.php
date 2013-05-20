bfe7b79ace4403ebfa78f23a0b38725683e9b5c7
cash <cash.costello@gmail.com>
2013-05-19 20:23:35 -0400

    remove relationship for user tags  - from @kevinjardine

--- actions/photos/image/untag.php
+++ actions/photos/image/untag.php
@@ -15,7 +15,13 @@ if (!$annotation->canEdit()) {
 	forward(REFERER);
 }
 
+$entity_guid = $annotation->entity_guid;
+$tag = unserialize($annotation->value);
+
 if ($annotation->delete()) {
+	if ($tag->type == 'user') {
+		remove_entity_relationship($tag->value, 'phototag', $entity_guid);
+	}
 	system_message(elgg_echo("tidypics:phototagging:delete:success"));
 } else {
 	system_message(elgg_echo("tidypics:phototagging:delete:error"));
